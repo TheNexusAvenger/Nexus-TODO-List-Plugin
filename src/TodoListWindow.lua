8eeab8823f85194dbfb0b84acc469bb90b6c2ea5
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-02-26 16:13:37 -0500

    Create initial window UI.

--- src/TodoListWindow.lua
+++ src/TodoListWindow.lua
@@ -20,7 +20,34 @@ function TodoListWindow:__new(Plugin)
     self.Title = "TODO List"
     self.Name = "TODO List"
 
-    --TODO: Contents
+    --Create the background.
+    local Background = NexusPluginComponents.new("Frame")
+    Background.Size = UDim2.new(1, -2, 1, -1)
+    Background.Position = UDim2.new(0, 1, 0, 0)
+    Background.Parent = self
+
+    --Create the top bar.
+    local TopBar = NexusPluginComponents.new("Frame")
+    TopBar.BorderSizePixel = 1
+    TopBar.Size = UDim2.new(1, 0, 0, 27)
+    TopBar.Parent = Background
+
+    local SearchBar = NexusPluginComponents.new("TextBox")
+    SearchBar.Size = UDim2.new(1, -2, 0, 19)
+    SearchBar.Position = UDim2.new(0, 1, 0, 1)
+    SearchBar.PlaceholderText = "Filter list"
+    SearchBar.Text = ""
+    SearchBar.Parent = TopBar
+    self:DisableChangeReplication("SearchBar")
+    self.SearchBar = SearchBar
+
+    --Create the scrolling frame.
+    local ScrollingFrame = NexusPluginComponents.new("ScrollingFrame")
+	ScrollingFrame.Position = UDim2.new(0, 0, 0, 28)
+	ScrollingFrame.Size = UDim2.new(1, 0, 1, -28)
+	ScrollingFrame.Parent = Background
+    self:DisableChangeReplication("ScrollingFrame")
+    self.ScrollingFrame = SearchBar
 end
 
 
