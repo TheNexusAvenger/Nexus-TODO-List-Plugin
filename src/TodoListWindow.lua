1569e89edfd3a24a59f85cb44e120d6da2180e60
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-02-26 16:46:37 -0500

    Add prompt to start in play test.

--- src/TodoListWindow.lua
+++ src/TodoListWindow.lua
@@ -4,6 +4,8 @@ TheNexusAvenger
 Window of the TODO list plugin.
 --]]
 
+local RunService = game:GetService("RunService")
+
 local NexusPluginComponents = require(script.Parent:WaitForChild("NexusPluginComponents"))
 local PluginInstance = NexusPluginComponents:GetResource("Base.PluginInstance")
 local TodoListEntry = require(script.Parent:WaitForChild("TodoListEntry"))
@@ -20,12 +22,16 @@ function TodoListWindow:__new(Plugin)
     self:InitializeSuper(Plugin:CreateDockWidgetPluginGui("TODO List", DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Bottom, false, false, 300, 100, 200, 50)))
     self.Title = "TODO List"
     self.Name = "TODO List"
+    TodoListEntry.Plugin = Plugin
 
     --Create the background.
     local Background = NexusPluginComponents.new("Frame")
     Background.Size = UDim2.new(1, -2, 1, -1)
     Background.Position = UDim2.new(0, 1, 0, 0)
+    Background.BorderSizePixel = 1
     Background.Parent = self
+    self:DisableChangeReplication("Background")
+    self.Background = Background
 
     --Create the top bar.
     local TopBar = NexusPluginComponents.new("Frame")
@@ -42,14 +48,61 @@ function TodoListWindow:__new(Plugin)
     self:DisableChangeReplication("SearchBar")
     self.SearchBar = SearchBar
 
+    --Start the list.
+    task.spawn(function()
+        if RunService:IsRunning() then
+            self:PromptToContinue()
+        end
+        self:InitializeList()
+    end)
+end
+
+--[[
+Prompts to continue before initializing.
+--]]
+function TodoListWindow:PromptToContinue()
+    --Create the button.
+    local ContinueButton = NexusPluginComponents.new("TextButton")
+    ContinueButton.BackgroundColor3 = Enum.StudioStyleGuideColor.DialogMainButton
+    ContinueButton.BorderSizePixel = 0
+    ContinueButton.Size = UDim2.new(0, 100, 0, 24)
+    ContinueButton.AnchorPoint = Vector2.new(0.5, 0)
+    ContinueButton.Position = UDim2.new(0.5, 0, 0, 50)
+    ContinueButton.Text = "Continue"
+    ContinueButton.TextColor3 = Enum.StudioStyleGuideColor.DialogMainButtonText
+    ContinueButton.Parent = self.Background
+
+    --Create the text.
+    local TextLabel = NexusPluginComponents.new("TextLabel")
+    TextLabel.Size = UDim2.new(0.8, 0, 0, 200)
+    TextLabel.AnchorPoint = Vector2.new(0.5, 0)
+    TextLabel.Position = UDim2.new(0.5, 0, 0, 78)
+    TextLabel.Text = "The list is disabled in run mode to save resources."
+    TextLabel.TextColor3 = Enum.StudioStyleGuideColor.SubText
+    TextLabel.TextXAlignment = Enum.TextXAlignment.Center
+    TextLabel.TextYAlignment = Enum.TextYAlignment.Top
+    TextLabel.TextWrapped = true
+    TextLabel.Parent = self.Background
+
+    --Wait for the button to be pressed.
+    ContinueButton.MouseButton1Down:Wait()
+
+    --Desroy the text and button.
+    ContinueButton:Destroy()
+    TextLabel:Destroy()
+end
+
+--[[
+Initailizes the list.
+--]]
+function TodoListWindow:InitializeList()
     --Create the scrolling frame.
-    TodoListEntry.Plugin = Plugin
     local ScrollingFrame = NexusPluginComponents.new("ScrollingFrame")
-	ScrollingFrame.Position = UDim2.new(0, 0, 0, 28)
-	ScrollingFrame.Size = UDim2.new(1, 0, 1, -28)
-	ScrollingFrame.Parent = Background
+    ScrollingFrame.Position = UDim2.new(0, 0, 0, 28)
+    ScrollingFrame.Size = UDim2.new(1, 0, 1, -28)
+    ScrollingFrame.Parent = self.Background
     self:DisableChangeReplication("ScrollingFrame")
-    self.ScrollingFrame = SearchBar
+    self.ScrollingFrame = ScrollingFrame
 
     self:DisableChangeReplication("SelectionList")
     self.SelectionList = NexusPluginComponents.new("SelectionList")
